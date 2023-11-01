27e5c6ddc28274fa8f37e6da4c8e87d0cf3bcf7f
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2023-11-01 21:11:31 +0100

    Update lxardoscope_wemos_d1_mini_vfast.ino
    made it faster

--- code/lxardoscope_wemos_d1_mini_vfast/lxardoscope_wemos_d1_mini_vfast.ino
+++ code/lxardoscope_wemos_d1_mini_vfast/lxardoscope_wemos_d1_mini_vfast.ino
@@ -1,18 +1,28 @@
 #define BUFSIZE 256
+#include "user_interface.h"
 uint8_t cbuf[3] = {0x80, 0x00, 0x00};
 boolean cal1 = false;
 uint16_t val[2];
+int value;
+
+#define num_samples 1
+uint16_t adc_addr[num_samples]; // point to the address of ADC continuously fast sampling output
+uint16_t adc_num = num_samples; // sampling number of ADC continuously fast sampling, range [1, 65535]
+uint8_t adc_clk_div = 8;        // ADC working clock = 80M/adc_clk_div, range [8, 32], the recommended value is 8
 
 void setup()
 {
   Serial.begin(2000000);
 }
 
-
-
 void loop()
 {
-  val[0] = (analogRead(A0) / 2.046);
+
+  system_adc_read_fast(adc_addr, adc_num, adc_clk_div);
+
+  value = (adc_addr[0]);
+
+  val[0] = (value / 2.046);
   if (cal1 == true)
   {
     val[0] = 0;

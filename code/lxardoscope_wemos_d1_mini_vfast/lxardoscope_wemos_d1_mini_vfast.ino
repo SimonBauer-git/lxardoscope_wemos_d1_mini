759d3d1821533c67aad9fb4b718a7c770d335e28
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2023-11-03 16:53:53 +0100

    Update lxardoscope_wemos_d1_mini_vfast.ino
    now up to 34ksps/16.5khz frequency (stable)

--- code/lxardoscope_wemos_d1_mini_vfast/lxardoscope_wemos_d1_mini_vfast.ino
+++ code/lxardoscope_wemos_d1_mini_vfast/lxardoscope_wemos_d1_mini_vfast.ino
@@ -1,49 +1,24 @@
-#define BUFSIZE 256
 #include "user_interface.h"
-uint8_t cbuf[3] = {0x80, 0x00, 0x00};
-boolean cal1 = false;
+#include <ESP8266WiFi.h>
+uint8_t cbuf[2] = {0x80, 0x00};
 uint16_t val[2];
-int value;
 
 #define num_samples 1
 uint16_t adc_addr[num_samples]; // point to the address of ADC continuously fast sampling output
 uint16_t adc_num = num_samples; // sampling number of ADC continuously fast sampling, range [1, 65535]
-uint8_t adc_clk_div = 8;        // ADC working clock = 80M/adc_clk_div, range [8, 32], the recommended value is 8
-
+uint8_t adc_clk_div = 1;        // ADC working clock = 80M/adc_clk_div
 void setup()
 {
+  WiFi.mode(WIFI_OFF);
   Serial.begin(2000000);
+  ets_intr_lock(); // close interrupt
+  noInterrupts();
 }
-
 void loop()
 {
 
   system_adc_read_fast(adc_addr, adc_num, adc_clk_div);
-
-  value = (adc_addr[0]);
-
-  val[0] = (value / 2.046);
-  if (cal1 == true)
-  {
-    val[0] = 0;
-  }
-  cbuf[0] = 0x80 | val[0] >> 3;
-  cbuf[1] = (val[0] << 4) & 0x70;
+  cbuf[0] = 0x80 | adc_addr[0] >> 3;
+  cbuf[1] = (adc_addr[0] << 4) & 0x70;
   Serial.write(cbuf, 2);
-
-  if (Serial.available())
-  {
-    char inChar = (char)Serial.read();
-    if (inChar == '1')
-    { // cal channel 1
-      // AC1 off
-      cal1 = true;
-    }
-
-    else if (inChar == '2')
-    { // cal channel 1
-      // AC1 off
-      cal1 = false;
-    }
-  }
 }

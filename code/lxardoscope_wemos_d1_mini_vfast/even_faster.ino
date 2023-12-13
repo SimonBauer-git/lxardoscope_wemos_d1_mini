9da49bccd8818bae593744667ea98df3ca18e5e9
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2023-12-13 22:59:36 +0100

    Update even_faster.ino

--- code/lxardoscope_wemos_d1_mini_vfast/even_faster.ino
+++ code/lxardoscope_wemos_d1_mini_vfast/even_faster.ino
@@ -1,1 +1,30 @@
-#inclife
+#include "user_interface.h"
+#include <ESP8266WiFi.h>
+uint8_t cbuf[2] = {0x80, 0x00};
+uint16_t val[2];
+
+#define num_samples 1
+uint16_t adc_addr[num_samples]; // point to the address of ADC continuously fast sampling output
+uint16_t adc_num = num_samples; // sampling number of ADC continuously fast sampling, range [1, 65535]
+uint8_t adc_clk_div = 1;        // ADC working clock = 80M/adc_clk_div
+
+void osc()
+{while(true)
+  {
+  system_adc_read_fast(adc_addr, adc_num, adc_clk_div);
+  cbuf[0] = 0x80 | adc_addr[0] >> 3;
+  cbuf[1] = (adc_addr[0] << 4) & 0x70;
+  Serial.write(cbuf, 2);}
+}
+
+void setup()
+{
+  WiFi.mode(WIFI_OFF);
+  Serial.begin(2000000);
+  ets_intr_lock(); // close interrupt
+  noInterrupts();
+}
+void loop()
+{
+osc();
+}

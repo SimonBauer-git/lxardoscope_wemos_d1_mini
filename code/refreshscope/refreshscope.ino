353299771a602efdf5409d7c846270f5fe08fa46
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2024-05-27 21:42:29 +0200

    Update refreshscope.ino

--- code/refreshscope/refreshscope.ino
+++ code/refreshscope/refreshscope.ino
@@ -1,7 +1,6 @@
-// alternative approach, instead of continously sending data out, capture a number of samples and then send that in bulk
 #include <ESP8266WiFi.h>
 
-uint16_t adc_addr[500];
+uint16_t adc_addr[16384];
 
 void setup() {
   WiFi.mode(WIFI_OFF);
@@ -16,11 +15,12 @@ void loop() {
 void scope()  //have an extra function that does the looping as this is faster than void loop
 {
   while (true) {
-    system_adc_read_fast(adc_addr, 500, 1); //adc_addr is where the Samples are stored, the next thing is the number of readings
-    int i = 0;
-    while (i<500){               //send the data out after taking 500 readings
+    system_adc_read_fast(adc_addr, 16384, 1);
+    int i =0;
+    while (i<16384){ 
     Serial.write(0x80 | adc_addr[i] >> 4);
+
     Serial.write((adc_addr[i] << 3) & 0x70);
-    i++;}
-  }
+    i++;
+  }}
 }
